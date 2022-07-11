7e4c44e6bd745ce638704838c147f8901173040c
emanueljoos <emanuel.joos>
2022-07-11 11:30:18 +0200

    add env variables

--- axh-lab-appl-streaming-func/azure_function_streaming.py
+++ axh-lab-appl-streaming-func/azure_function_streaming.py
@@ -8,14 +8,11 @@ import pymsteams
 import os
 import pandas as pd
 from azure.storage.blob import BlobServiceClient
-from io import BytesIO
 from dotenv import load_dotenv
 from datetime import date
-
 from .timescale_client import TimescaleClient
 
 load_dotenv()
-
 
 
 class AzureFunctionStreaming:
@@ -30,8 +27,8 @@ class AzureFunctionStreaming:
 
         # undefined Sensors
         self.undefined_sensors = pd.DataFrame(columns=[['SensorName', 'Plant']])
-        self.myTeamsMessage = pymsteams.connectorcard("https://axpogrp.webhook.office.com/webhookb2/1c7d8e30-f530-4faf-acc0-7ef098a2a388@8619c67c-945a-48ae-8e77-35b1b71c9b98/IncomingWebhook/5b6d6935cb774847950f3f9cd081c0cb/adfeab72-7d9c-4e19-a21b-6781b139b707")
-        self.myTeamsMessage.addLinkButton("Check this container for missing sensors", "https://portal.azure.com/#view/Microsoft_Azure_Storage/ContainerMenuBlade/~/overview/storageAccountId/%2Fsubscriptions%2F58b564b2-e2b7-4d4b-b168-5554c5a2aa5f%2FresourceGroups%2Faxh-lab-appl-streaming-rg%2Fproviders%2FMicrosoft.Storage%2FstorageAccounts%2Faxh4lab4streaming4sa/path/missing-sensors/defaultId/properties/publicAccessVal/None")
+        self.myTeamsMessage = pymsteams.connectorcard(os.getenv("TEAMS_URL"))
+        self.myTeamsMessage.addLinkButton("Check this container for missing sensors", os.getenv("LINK_TO_STORAGE_ACCOUNT"))
         connection_string = os.getenv("AZURE_STORAGE_CONNECTIONSTRING")
         self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         self.blob_client = self.blob_service_client.get_blob_client(container=os.getenv("AZURE_CONTAINER_NAME"), blob=f"missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv")
