c03c5cdc1d2a1f7f4aa52f8cc30386372b929dd2
emanueljoos <emanuel.joos>
2022-08-25 13:49:54 +0200

    update streaming

--- axh-lab-appl-streaming-func/azure_function_streaming.py
+++ axh-lab-appl-streaming-func/azure_function_streaming.py
@@ -9,54 +9,26 @@ import pandas as pd
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 from datetime import date
-import requests
 from .timescale_client import TimescaleClient
+from .signal_client import SignalClient
 from keycloak.keycloak_openid import KeycloakOpenID  # type: ignore
 
 load_dotenv()
 
-# Set the logging level for all azure-* libraries
 logger = logging.getLogger('azure')
 logger.setLevel(logging.ERROR)
 logger.setLevel(logging.INFO)
 
 
-class SignalTable:
-    def __init__(self):
-        self.asset_api_base = "https://axh-stage-appl-app-asset-api.azurewebsites.net"
-        self.data = {
-            "grant_type": "client_credentials",
-            "client_id": os.getenv("KEYCLOAK_CLIENT_ID"),
-            "client_secret": os.getenv("CLIENT_SECRET"),
-        }
-        self.auth_header = {"Authorization": "Bearer " + self.provide_token()}
-
-    def provide_token(self):
-        res = requests.post(
-            "https://accounts.withaxpo.com/auth/realms/axh/protocol/openid-connect/token",
-            data=self.data,
-        )
-        return res.json()["access_token"]
-
-    def provide_signal_data(self):
-        res = requests.get(f"{self.asset_api_base}/signals?limit={10}&offset={0}", headers=self.auth_header)
-        data = res.json()
-        return pd.json_normalize(data['records'])
-
-
 class AzureFunctionStreaming:
     def __init__(self) -> None:
         # Load data
-
-        sensors = pd.read_csv("Sensor.csv")
-        reduced_sensor_data = sensors[['SensorId', 'SensorName', 'Plant']]
-        self.sensor_table = reduced_sensor_data
-        reduced_sensor_data["Unique"] = reduced_sensor_data['SensorName'] + reduced_sensor_data['Plant']
-        reduced_sensor_data["Hash"] = reduced_sensor_data['Unique'].apply(hash)
-        self.hash_table = pd.DataFrame(index=reduced_sensor_data["Hash"], columns=['SensorId'], data=reduced_sensor_data['SensorId'].values)
+        signal_client = SignalClient()
+        self.hash_table = signal_client.provide_hash_table()
+        logger.info("Signal Table Loaded")
 
         # undefined Sensors
-        self.undefined_sensors = pd.DataFrame(columns=[['SensorName', 'Plant']])
+        self.undefined_sensors = pd.DataFrame(columns=[['control_system_identifier', 'plant']])
         self.myTeamsMessage = pymsteams.connectorcard(os.getenv("TEAMS_URL"))
         self.myTeamsMessage.addLinkButton("Check this container for missing sensors", os.getenv("LINK_TO_STORAGE_ACCOUNT"))
         connection_string = os.getenv("AzureWebJobsStorage")
@@ -73,30 +45,29 @@ class AzureFunctionStreaming:
         logger.info(f"Name: {myblob.name}  "
                     f"Blob Size: {myblob.length} bytes  ")
         
-        jsonData=json.load(myblob)
+        json_data = json.load(myblob)
         values = []
         send_message = False
-        for entry in jsonData:
+        for entry in json_data:
+            name = entry["NodeId"].partition('#s=')[2].replace('%3a', ':').replace('%2f', '/')
+            plant = entry["DisplayName"].partition("_")[0]
             try:
                 values.append(
                     (
                         pd.to_datetime(entry['Value']['SourceTimestamp']).to_pydatetime(), 
-                        self.hash_table.loc[hash(entry["NodeId"].partition(';s=')[2]+entry["DisplayName"].partition("_")[0])].values[0],
+                        self.hash_table.loc[hash(name + plant)].values[0],
                         entry['Value']['Value'] 
                     )
                 )
-            except:
+            except KeyError:
                 try:
-                    self.undefined_sensors.loc[hash(entry["NodeId"].partition(';s=')[2]+entry["DisplayName"].partition("_")[0])]
-                except:
-                    if entry == 'events':
-                        pass
-                    else:
-                        logger.info(f"Missing Sensor coming from OPC: {entry['NodeId'].partition(';s=')[2]}")
+                    self.undefined_sensors.loc[hash(name + plant)]
+                except KeyError:
+                        logger.info(f"Missing Sensor coming from OPC: {name}")
                         logger.info(f"Number of undefined Sensors coming from OPC: {self.undefined_sensors.shape[0]}")
-                        self.undefined_sensors.loc[hash(entry["NodeId"].partition(';s=')[2]+entry["DisplayName"].partition("_")[0])] = [
-                            entry["NodeId"].partition(';s=')[2], 
-                            entry["DisplayName"].partition("_")[0]
+                        self.undefined_sensors.loc[hash(name + plant)] = [
+                            name,
+                            plant
                         ]
                         send_message = True
 
@@ -104,25 +75,16 @@ class AzureFunctionStreaming:
             self.blob_client.upload_blob(data=self.undefined_sensors.to_csv(), overwrite=True)
             self.myTeamsMessage.text(f"New missing sensor is added to missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv")
             self.myTeamsMessage.send()
-            
-        # upload
         try:
             self.conn.isolation_level
         except OperationalError as oe:
             timescale_client = TimescaleClient()
             self.conn = timescale_client.get_connection()
-            logger.info("New connection needs to be established")
-        try: 
-            self.mgr.copy(values)
-            self.conn.commit()
-            logger.info(f"Uploading blob {myblob.name} was successful")
-        except:
-            try:
-                self.conn.rollback()
-                self.mgr.copy(values)
-                self.conn.commit()
-            except Exception as err:
-                logger.error(f"Exception occured during upload {err.message}")
+            logger.info(f"New connection needs to be established: {oe}")
+        self.mgr.copy(values)
+        self.conn.commit()
+
+        logger.info(f"Uploading blob {myblob.name} was successful")
 
 
 
