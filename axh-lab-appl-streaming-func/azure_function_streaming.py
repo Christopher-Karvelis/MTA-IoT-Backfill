f6ef9f5f7cce5dd801ec4a46ae7fe519cb0ccea2
emanueljoos <emanuel.joos>
2022-08-25 13:55:51 +0200

    add pre commit

--- axh-lab-appl-streaming-func/azure_function_streaming.py
+++ axh-lab-appl-streaming-func/azure_function_streaming.py
@@ -1,21 +1,22 @@
-import logging
 import json
-import azure.functions as func
-from pgcopy import CopyManager
-from psycopg2 import OperationalError
-import pymsteams
+import logging
 import os
+from datetime import date
+
+import azure.functions as func
 import pandas as pd
+import pymsteams
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
-from datetime import date
-from .timescale_client import TimescaleClient
+from pgcopy import CopyManager
+from psycopg2 import OperationalError
+
 from .signal_client import SignalClient
-from keycloak.keycloak_openid import KeycloakOpenID  # type: ignore
+from .timescale_client import TimescaleClient
 
 load_dotenv()
 
-logger = logging.getLogger('azure')
+logger = logging.getLogger("azure")
 logger.setLevel(logging.ERROR)
 logger.setLevel(logging.INFO)
 
@@ -28,52 +29,71 @@ class AzureFunctionStreaming:
         logger.info("Signal Table Loaded")
 
         # undefined Sensors
-        self.undefined_sensors = pd.DataFrame(columns=[['control_system_identifier', 'plant']])
+        self.undefined_sensors = pd.DataFrame(
+            columns=[["control_system_identifier", "plant"]]
+        )
         self.myTeamsMessage = pymsteams.connectorcard(os.getenv("TEAMS_URL"))
-        self.myTeamsMessage.addLinkButton("Check this container for missing sensors", os.getenv("LINK_TO_STORAGE_ACCOUNT"))
+        self.myTeamsMessage.addLinkButton(
+            "Check this container for missing sensors",
+            os.getenv("LINK_TO_STORAGE_ACCOUNT"),
+        )
         connection_string = os.getenv("AzureWebJobsStorage")
-        self.blob_service_client = BlobServiceClient.from_connection_string(connection_string, logging_enable=False)
-        self.blob_client = self.blob_service_client.get_blob_client(container=os.getenv("AZURE_CONTAINER_NAME"), blob=f"missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv")
+        self.blob_service_client = BlobServiceClient.from_connection_string(
+            connection_string, logging_enable=False
+        )
+        self.blob_client = self.blob_service_client.get_blob_client(
+            container=os.getenv("AZURE_CONTAINER_NAME"),
+            blob=f"missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv",
+        )
 
         # Timescale
         timescale_client = TimescaleClient()
         self.conn = timescale_client.get_connection()
-        cols = ['ts', 'sensorid', 'measurementvalue']
-        self.mgr = CopyManager(self.conn, 'measurements', cols)
+        cols = ["ts", "sensorid", "measurementvalue"]
+        self.mgr = CopyManager(self.conn, "measurements", cols)
 
     async def input(self, myblob: func.InputStream):
-        logger.info(f"Name: {myblob.name}  "
-                    f"Blob Size: {myblob.length} bytes  ")
-        
+        logger.info(f"Name: {myblob.name}  " f"Blob Size: {myblob.length} bytes  ")
+
         json_data = json.load(myblob)
         values = []
         send_message = False
         for entry in json_data:
-            name = entry["NodeId"].partition('#s=')[2].replace('%3a', ':').replace('%2f', '/')
+            name = (
+                entry["NodeId"]
+                .partition("#s=")[2]
+                .replace("%3a", ":")
+                .replace("%2f", "/")
+            )
             plant = entry["DisplayName"].partition("_")[0]
             try:
                 values.append(
                     (
-                        pd.to_datetime(entry['Value']['SourceTimestamp']).to_pydatetime(), 
+                        pd.to_datetime(
+                            entry["Value"]["SourceTimestamp"]
+                        ).to_pydatetime(),
                         self.hash_table.loc[hash(name + plant)].values[0],
-                        entry['Value']['Value'] 
+                        entry["Value"]["Value"],
                     )
                 )
             except KeyError:
                 try:
                     self.undefined_sensors.loc[hash(name + plant)]
                 except KeyError:
-                        logger.info(f"Missing Sensor coming from OPC: {name}")
-                        logger.info(f"Number of undefined Sensors coming from OPC: {self.undefined_sensors.shape[0]}")
-                        self.undefined_sensors.loc[hash(name + plant)] = [
-                            name,
-                            plant
-                        ]
-                        send_message = True
+                    logger.info(f"Missing Sensor coming from OPC: {name}")
+                    logger.info(
+                        f"Number of undefined Sensors coming from OPC: {self.undefined_sensors.shape[0]}"
+                    )
+                    self.undefined_sensors.loc[hash(name + plant)] = [name, plant]
+                    send_message = True
 
         if send_message:
-            self.blob_client.upload_blob(data=self.undefined_sensors.to_csv(), overwrite=True)
-            self.myTeamsMessage.text(f"New missing sensor is added to missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv")
+            self.blob_client.upload_blob(
+                data=self.undefined_sensors.to_csv(), overwrite=True
+            )
+            self.myTeamsMessage.text(
+                f"New missing sensor is added to missing-sensors-{date.today().strftime('%Y-%m-%d')}.csv"
+            )
             self.myTeamsMessage.send()
         try:
             self.conn.isolation_level
@@ -85,8 +105,3 @@ class AzureFunctionStreaming:
         self.conn.commit()
 
         logger.info(f"Uploading blob {myblob.name} was successful")
-
-
-
-
-
