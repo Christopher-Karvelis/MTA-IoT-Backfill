3d634ddf8bae7ed3f18c57a245fa7b581e30a63d
emanueljoos <emanuel.joos>
2022-08-26 10:51:33 +0200

    remove debugger

--- axh-lab-appl-streaming-func/__init__.py
+++ axh-lab-appl-streaming-func/__init__.py
@@ -1,8 +1,9 @@
 import azure.functions as func
+
 from .azure_function_streaming import AzureFunctionStreaming
 
-import pydevd_pycharm
-pydevd_pycharm.settrace('127.0.0.1', port=9091, stdoutToServer=True, stderrToServer=True)
+# import pydevd_pycharm
+# pydevd_pycharm.settrace('127.0.0.1', port=9091, stdoutToServer=True, stderrToServer=True)
 
 azure_streaming = AzureFunctionStreaming()
 
