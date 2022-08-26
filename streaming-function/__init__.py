6170519c0540a55ae435deef2fb7dd62c4d29111
emanueljoos <emanuel.joos>
2022-08-26 13:18:51 +0200

    rename func

---
+++ streaming-function/__init__.py
@@ -0,0 +1,12 @@
+import azure.functions as func
+
+from .azure_function_streaming import AzureFunctionStreaming
+
+# import pydevd_pycharm
+# pydevd_pycharm.settrace('127.0.0.1', port=9091, stdoutToServer=True, stderrToServer=True)
+
+azure_streaming = AzureFunctionStreaming()
+
+
+async def main(myblob: func.InputStream):
+    await azure_streaming.input(myblob)
--- streaming-function/signal_client.py
+++ streaming-function/signal_client.py
@@ -1,16 +1,17 @@
-import requests
 import os
-from dotenv import load_dotenv
-import pandas as pd
 from typing import List
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class SignalClient:
     def __init__(self):
-        self._columns_to_keep = ['signal_id', 'control_system_identifier', 'plant']
-        self._asset_api_base = os.getenv('ASSET_API_BASE')
+        self._columns_to_keep = ["signal_id", "control_system_identifier", "plant"]
+        self._asset_api_base = os.getenv("ASSET_API_BASE")
         self._data = {
             "grant_type": "client_credentials",
             "client_id": os.getenv("KEYCLOAK_CLIENT_ID"),
@@ -28,27 +29,35 @@ class SignalClient:
         return res.json()["access_token"]
 
     def _provide_signal_data(self, power_plant: str) -> pd.DataFrame:
-        res = requests.get(f"{self._asset_api_base}/signals?limit={100000}&offset={0}&plant={power_plant}",
-                           headers=self._auth_header)
+        res = requests.get(
+            f"{self._asset_api_base}/signals?limit={100000}&offset={0}&plant={power_plant}",
+            headers=self._auth_header,
+        )
         if res.status_code != 200:
-            raise Exception(f"Data request failed with status code {res.status_code} "
-                            f"and details: {res.json()}")
+            raise Exception(
+                f"Data request failed with status code {res.status_code} "
+                f"and details: {res.json()}"
+            )
         signals = res.json()
-        return pd.json_normalize(signals['records'])
+        return pd.json_normalize(signals["records"])
 
     def _provide_power_plants(self) -> List[str]:
-        res = requests.get(f"{self._asset_api_base}/assets?asset_type_identifier=power_plant",
-                           headers=self._auth_header)
+        res = requests.get(
+            f"{self._asset_api_base}/assets?asset_type_identifier=power_plant",
+            headers=self._auth_header,
+        )
         if res.status_code != 200:
-            raise Exception(f"Data request failed with status code {res.status_code} "
-                            f"and details: {res.json()}")
-        return [i['name'] for i in res.json()]
+            raise Exception(
+                f"Data request failed with status code {res.status_code} "
+                f"and details: {res.json()}"
+            )
+        return [i["name"] for i in res.json()]
 
     def _provide_signal_table_with_power_plants(self) -> pd.DataFrame:
         signal_list = []
         for plant in self._power_plants:
             data = self._provide_signal_data(plant)
-            data['plant'] = plant
+            data["plant"] = plant
             signal_list.append(data)
 
         return pd.concat(signal_list, ignore_index=True)
@@ -56,11 +65,14 @@ class SignalClient:
     def provide_hash_table(self) -> pd.DataFrame:
         reduced_sensor_data = self._signals[self._columns_to_keep]
         reduced_sensor_data_1 = reduced_sensor_data.copy()
-        reduced_sensor_data_1["Unique"] = reduced_sensor_data['control_system_identifier'] + reduced_sensor_data['plant']
+        reduced_sensor_data_1["Unique"] = (
+            reduced_sensor_data["control_system_identifier"]
+            + reduced_sensor_data["plant"]
+        )
         reduced_sensor_data_2 = reduced_sensor_data_1.copy()
-        reduced_sensor_data_2["Hash"] = reduced_sensor_data_1['Unique'].apply(hash)
-        return pd.DataFrame(index=reduced_sensor_data_2["Hash"],
-                            columns=['signal_id'],
-                            data=reduced_sensor_data_2['signal_id'].values)
-
-
+        reduced_sensor_data_2["Hash"] = reduced_sensor_data_1["Unique"].apply(hash)
+        return pd.DataFrame(
+            index=reduced_sensor_data_2["Hash"],
+            columns=["signal_id"],
+            data=reduced_sensor_data_2["signal_id"].values,
+        )
