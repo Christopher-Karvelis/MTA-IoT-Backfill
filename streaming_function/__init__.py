a6f1388d5c7c181adbc85bb33a98a8196a0cae2f
emanueljoos <emanuel.joos>
2022-08-29 17:29:03 +0200

    Add property for reloading

--- streaming_function/__init__.py
+++ streaming_function/__init__.py
@@ -5,8 +5,8 @@ import azure.functions as func
 from .azure_function_streaming import AzureFunctionStreaming
 
 # import pydevd_pycharm
-
-
+#
+#
 # pydevd_pycharm.settrace(
 #     "127.0.0.1", port=9091, stdoutToServer=True, stderrToServer=True
 # )
