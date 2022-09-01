75551bafc652562b4512dd484849faa8f77323c5
emanueljoos <emanuel.joos>
2022-09-01 11:28:42 +0200

    fix bug when new hour is switched

--- streaming_function/__init__.py
+++ streaming_function/__init__.py
@@ -1,19 +1,18 @@
 import logging
 
 import azure.functions as func
+import pydevd_pycharm
 
 from .azure_function_streaming import AzureFunctionStreaming
 
-# import pydevd_pycharm
-#
-#
-# pydevd_pycharm.settrace(
-#     "127.0.0.1", port=9091, stdoutToServer=True, stderrToServer=True
-# )
+pydevd_pycharm.settrace(
+    "127.0.0.1", port=9091, stdoutToServer=True, stderrToServer=True
+)
 
 azure_streaming = AzureFunctionStreaming()
 
 
 async def main(myblob: func.InputStream):
     logging.info("Python Blob trigger function processed %s", myblob.name)
-    await azure_streaming.input(myblob)
+    if myblob.length > 1:
+        await azure_streaming.input(myblob)
