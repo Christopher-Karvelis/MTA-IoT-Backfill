4a184eaa39be670553ab38f1f12f444cc5cf617e
emanueljoos <emanuel.joos>
2022-09-01 13:13:46 +0200

    fix bug

--- streaming_function/__init__.py
+++ streaming_function/__init__.py
@@ -4,6 +4,7 @@ import azure.functions as func
 
 from .azure_function_streaming import AzureFunctionStreaming
 
+#
 # import pydevd_pycharm
 # pydevd_pycharm.settrace(
 #     "127.0.0.1", port=9091, stdoutToServer=True, stderrToServer=True
@@ -14,7 +15,7 @@ azure_streaming = AzureFunctionStreaming()
 
 async def main(myblob: func.InputStream):
     logging.info("Python Blob trigger function processed %s", myblob.name)
-    if myblob.length > 1:
+    if myblob is not None:
         await azure_streaming.input(myblob)
     else:
         pass
