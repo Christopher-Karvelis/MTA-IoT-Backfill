a3b19dc94940ac4ca437b524aaee56de0c879908
emanueljoos <emanuel.joos@axpo.com>
2023-10-19 08:39:27 +0200

    Add loggingn again to see if data uploaded is older then 24h

--- streaming_function/azure_function_streaming.py
+++ streaming_function/azure_function_streaming.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from datetime import datetime, timedelta, timezone
 
 import asyncpg
 import azure.functions as func
@@ -35,6 +36,8 @@ class AzureFunctionStreaming:
         self.port = os.getenv("TIMESCALE_PORT")
         self.dbname = os.getenv("TIMESCALE_DATABASE_NAME")
 
+        self.time_accepted = 24
+
     async def input(self, jsonblob: func.InputStream):
 
         conn = await asyncpg.connect(
@@ -48,6 +51,8 @@ class AzureFunctionStreaming:
         rejected_by_streaming = []
         plants_processed = {}
         not_numbers_per_power_plant = {}
+        time_now = datetime.now(timezone.utc)
+        time_too_old_per_power_plant = {}
         for entry in json_data:
             control_system_identifier = entry["control_system_identifier"]
             plant = entry["plant"]
@@ -72,6 +77,13 @@ class AzureFunctionStreaming:
                         entry["measurement_value"]
                     ]
 
+                if pd.to_datetime(entry["ts"]).to_pydatetime() <= time_now - timedelta(
+                        hours=self.time_accepted
+                ):
+                    time_too_old_per_power_plant[plant] = (
+                            time_too_old_per_power_plant.get(plant, 0) + 1
+                    )
+
             except KeyError:
                 rejected_by_streaming.append(entry)
 
@@ -93,6 +105,7 @@ class AzureFunctionStreaming:
 
         plants_processed_reformatted = [{key: value} for key, value in plants_processed.items()]
         not_numbers_per_power_plant_reformatted = [{key: value} for key, value in not_numbers_per_power_plant.items()]
+        time_too_old_per_power_plant_reformatted = [{key: value} for key, value in time_too_old_per_power_plant.items()]
 
         logger.info(
             f"Name: {jsonblob.name}  "
@@ -101,6 +114,8 @@ class AzureFunctionStreaming:
             f"Number of values processed per plant {plants_processed_reformatted} \n"
             f"Number of values rejected i.e. not in our signal list {len(rejected_by_streaming)} \n "
             f"Data in blob is not a number {not_numbers_per_power_plant_reformatted} \n "
+            f"Data in blob is older then {self.time_accepted} "
+            f"hours {time_too_old_per_power_plant_reformatted} \n "
             f"Uploading blob {jsonblob.name} was successful \n"
             f"Uploaded {len(remove_nan)} to {jsonblob.name} -- "
             f"fraction of uploaded/processed {zero_div(len(remove_nan), len(json_data))}"
