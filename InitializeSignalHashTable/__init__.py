037ec24e23f6d549e587107191f6cd98ebb92337
Bastian Roehrig <bastian.roehrig@axpo.com>
2023-11-01 11:08:21 +0100

    write signal hash table to storage account for sharing with other activity functions

--- InitializeSignalHashTable/__init__.py
+++ InitializeSignalHashTable/__init__.py
@@ -1,17 +1,28 @@
 import os
 import json
 
+from azure.storage.blob import BlobServiceClient
+
 from InitializeSignalHashTable.signal_client import SignalClient
 
 
 def main(inputParameters: dict) -> str:
     target_connection_string = os.getenv("AzureWebJobsStorage")
-    target_storage_options = {"connection_string": target_connection_string}
 
     signal_client = SignalClient()
     hash_table = signal_client.provide_hash_table()
-    with open("signal_dictionary.json", "w") as signal_file:
-        json.dump(hash_table, signal_file)
+
+    blob_service_client = BlobServiceClient.from_connection_string(
+        target_connection_string
+    )
+    container_client = blob_service_client.get_container_client(
+        container="backfill"
+    )
+    blob_name = f'signal_hash_table'
+    blob_client = container_client.get_blob_client(blob=blob_name)
+
+    data = json.dumps(hash_table)
+
+    blob_client.upload_blob(data)
 
     return "Success"
-
