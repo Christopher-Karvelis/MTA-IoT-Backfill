45e5dc5daf7b2750ba72517de45792d992399f6e
Bastian Roehrig <bastian.roehrig@axpo.com>
2023-11-07 10:57:31 +0100

    value -> measurement value

--- tests/test_parse_jsons.py
+++ tests/test_parse_jsons.py
@@ -7,17 +7,17 @@ class TestParseJsons:
 
     def test_turn_result_tuple_into_dataframe_add_origin_information(self):
         result_df = turn_result_tuple_into_dataframe(
-            ("my_origin", json.dumps([{"value": 0, "ts": "2022-10-10"}])))
+            ("my_origin", json.dumps([{"measurement_value": 0, "ts": "2022-10-10"}])))
 
         assert result_df["source"][0] == "my_origin"
 
     def test_turn_result_tuple_into_dataframe_coerces_numeric(self):
         result_df = turn_result_tuple_into_dataframe(
             ("my_origin", json.dumps(
-                [{"value": 0, "ts": "2022-10-10"},
-                 {"value": "wat", "ts": "2022-11-11"},
-                 {"value": True, "ts": "2022-11-11"}])))
+                [{"measurement_value": 0, "ts": "2022-10-10"},
+                 {"measurement_value": "wat", "ts": "2022-11-11"},
+                 {"measurement_value": True, "ts": "2022-11-11"}])))
 
-        assert result_df["value"][0] == 0.0
-        assert result_df["value"].isna()[1]
-        assert result_df["value"][2] == 1.0
+        assert result_df["measurement_value"][0] == 0.0
+        assert result_df["measurement_value"].isna()[1]
+        assert result_df["measurement_value"][2] == 1.0
